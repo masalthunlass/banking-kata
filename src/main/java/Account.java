c7b90af3901d471a8e94e61dab7ec1e58afe511c
masalthunlass <masalthunlass@gmail.com>
2021-12-19 17:58:17 +0100

    balance of 100 and deposit of 200 -> balance is 300

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -4,11 +4,19 @@ public class Account {
 
     private BigInteger balance = BigInteger.ZERO;
 
+    public Account() {
+
+    }
+    public Account(BigInteger balance) {
+        this.balance = balance;
+    }
+
     public BigInteger getBalance() {
         return balance;
     }
 
     public void depose(BigInteger amount) {
-         this.balance = new BigInteger("100");
+         this.balance = this.balance.add(amount);
     }
+
 }
--- src/test/java/AccountTest.java
+++ src/test/java/AccountTest.java
@@ -9,18 +9,27 @@ public class AccountTest {
 
 
     @Test
-    void given_a_new_account_the_balance_is_0() {
+    void given_a_new_account_then_the_balance_is_0() {
         final Account account = new Account();
         assertThat(account.getBalance()).isEqualTo(0);
     }
 
     @Test
-    void given_a_deposit_of_100_the_balance_is_100() {
+    void given_a_deposit_of_100_then_the_balance_is_100() {
         final Account account = new Account();
         final BigInteger amount = new BigInteger("100");
         account.depose(amount);
         assertThat(account.getBalance()).isEqualTo(new BigInteger("100"));
     }
+
+    @Test
+    void given_a_deposit_of_200_and_balance_is_100_then_the_balance_is_300() {
+        final Account account =new Account(new BigInteger("100"));
+        final BigInteger amount = new BigInteger("200");
+        account.depose(amount);
+        assertThat(account.getBalance()).isEqualTo(new BigInteger("300"));
+    }
+
 
 
 }
