8b67384cec7830f34bd4a480fcf2317199f2562f
masalthunlass <masalthunlass@gmail.com>
2021-12-26 17:30:06 +0100

    when creating account operation then balance must not be null

--- src/test/java/bank/AccountStatementTest.java
+++ src/test/java/bank/AccountStatementTest.java
@@ -14,27 +14,27 @@ class AccountStatementTest {
 
     @Test
     void should_print_operation_date_first_when_printing_deposit_operation() {
-        this.accountStatement.addDepositOperation(new BigInteger("100"), null, LocalDate.of(2020, 1, 1));
+        this.accountStatement.addDepositOperation(new BigInteger("100"), new BigInteger("100"), LocalDate.of(2020, 1, 1));
         assertThat(this.accountStatement.print()).startsWith("01/01/2020");
     }
 
     @Test
     void should_print_operation_date_first_when_printing_withdraw_operation() {
-        this.accountStatement.addWithdrawOperation(new BigInteger("100"), null, LocalDate.of(2020, 1, 3));
+        this.accountStatement.addWithdrawOperation(new BigInteger("100"), new BigInteger("100"), LocalDate.of(2020, 1, 3));
         assertThat(this.accountStatement.print()).startsWith("03/01/2020");
     }
 
 
     @Test
     void should_print_amount_second_when_printing_deposit_operation() {
-        this.accountStatement.addDepositOperation(new BigInteger("100"), null, LocalDate.of(2020, 1, 1));
+        this.accountStatement.addDepositOperation(new BigInteger("100"), new BigInteger("100"), LocalDate.of(2020, 1, 1));
         assertThat(this.accountStatement.print()).startsWith("01/01/2020|100");
     }
 
 
     @Test
     void should_print_amount_second_when_printing_withdraw_operation() {
-        this.accountStatement.addWithdrawOperation(new BigInteger("50"), null, LocalDate.of(2020, 1, 3));
+        this.accountStatement.addWithdrawOperation(new BigInteger("50"), new BigInteger("100"), LocalDate.of(2020, 1, 3));
         assertThat(this.accountStatement.print()).startsWith("03/01/2020|-50");
     }
 
@@ -65,6 +65,24 @@ class AccountStatementTest {
 
         });
     }
+
+    @Test
+    void should_not_create_a_withdraw_operation_with_null_balance() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            this.accountStatement.addWithdrawOperation(new BigInteger("250"),null, LocalDate.of(2020, 1, 3));
+
+        });
+    }
+
+
+    @Test
+    void should_not_create_a_deposit_operation_with_null_balance() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            this.accountStatement.addDepositOperation(new BigInteger("250"),null, LocalDate.of(2020, 1, 3));
+
+        });
+    }
+
 
 
 
